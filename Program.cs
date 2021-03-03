fb4625e0070c0fbae1f2223889b5507fdb98775e
Isaac <isaacgeazy@gmail.com>
2021-03-02 21:51:00 -0300

    Atividade Arcii Art

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Atividade_ArciiArt
+{
+    class Program
+       {         static void Main(string[] args)
+        {
+           Console.WriteLine(@"
+                                ||  || 
+                                \\()// 
+                               //(__)\\
+                               ||    || 
+            ");
+        }
+    }
+}
